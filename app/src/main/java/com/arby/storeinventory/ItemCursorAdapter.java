8e35f518ad1603385579564af71750929d7c768d
Rares Borcea <raresborcea@gmail.com>
2019-11-04 21:25:55 +0200

    Update ItemCursorAdapter.java

--- app/src/main/java/com/arby/storeinventory/ItemCursorAdapter.java
+++ app/src/main/java/com/arby/storeinventory/ItemCursorAdapter.java
@@ -21,9 +21,8 @@ import static com.arby.storeinventory.R.id.quantity;
 
 
 /**
- * {@link ItemCursorAdapter} is an adapter for a list or grid view
- * that uses a {@link Cursor} of item data as its data source. This adapter knows
- * how to create list items for each row of item data in the {@link Cursor}.
+ * ItemCursorAdapter is an adapter for a list or grid view
+ * that uses a Cursor of item data as its data source. 
  */
 public class ItemCursorAdapter extends CursorAdapter {
 
@@ -34,7 +33,7 @@ public class ItemCursorAdapter extends CursorAdapter {
      * Constructs a new {@link ItemCursorAdapter}.
      *
      * @param context The context
-     * @param c       The cursor from which to get the data.
+     * @param c       The cursor from which to get the data
      */
     public ItemCursorAdapter(CatalogActivity context, Cursor c) {
         super(context, c, 0 /* flags */);
@@ -42,13 +41,13 @@ public class ItemCursorAdapter extends CursorAdapter {
     }
 
     /**
-     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     * Makes a new blank list item view. No data is set (or bound) to the views yet
      *
      * @param context app context
      * @param cursor  The cursor from which to get the data. The cursor is already
-     *                moved to the correct position.
+     *                moved to the correct position
      * @param parent  The parent to which the new view is attached to
-     * @return the newly created list item view.
+     * @return the newly created list item view
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
@@ -56,14 +55,14 @@ public class ItemCursorAdapter extends CursorAdapter {
     }
 
     /**
-     * This method binds the item data (in the current row pointed to by cursor) to the given
+     * Binds the item data (in the current row pointed to by cursor) to the given
      * list item layout. For example, the name for the current item can be set on the name TextView
      * in the list item layout.
      *
      * @param view    Existing view, returned earlier by newView() method
      * @param context app context
      * @param cursor  The cursor from which to get the data. The cursor is already moved to the
-     *                correct row.
+     *                correct row
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
@@ -73,7 +72,6 @@ public class ItemCursorAdapter extends CursorAdapter {
         TextView priceView = (TextView) view.findViewById(R.id.price);
         Button buttonView = (Button) view.findViewById(R.id.button);
         LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.details);
-
 
         // Extract properties from cursor
         final long id = cursor.getLong(cursor.getColumnIndex(ItemEntry._ID));
@@ -101,7 +99,7 @@ public class ItemCursorAdapter extends CursorAdapter {
             }
         });
 
-        // Method used to redirect the user to the EditActivity by clicking the list item details
+        // Redirect the user to the EditActivity by clicking the list item details
         linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
