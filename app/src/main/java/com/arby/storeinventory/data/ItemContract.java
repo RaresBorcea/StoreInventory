3002f16579dff0a1b874daa8455bc82dc8bfb70c
Rares Borcea <raresborcea@gmail.com>
2019-11-04 21:28:25 +0200

    Update ItemContract.java

--- app/src/main/java/com/arby/storeinventory/data/ItemContract.java
+++ app/src/main/java/com/arby/storeinventory/data/ItemContract.java
@@ -12,53 +12,43 @@ import android.provider.BaseColumns;
 public final class ItemContract {
 
     // To prevent someone from accidentally instantiating the contract class,
-    // give it an empty constructor.
+    // give it an empty constructor
     private ItemContract() {
     }
 
     /**
      * The "Content authority" is a name for the entire content provider, similar to the
-     * relationship between a domain name and its website.  A convenient string to use for the
+     * relationship between a domain name and its website. A convenient string to use for the
      * content authority is the package name for the app, which is guaranteed to be unique on the
      * device.
      */
     public static final String CONTENT_AUTHORITY = "com.arby.storeinventory";
 
-    /**
-     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
-     * the content provider.
-     */
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
 
     public static final String PATH_STORE = "store";
 
     /**
-     * Inner class that defines constant values for the store database table.
-     * Each entry in the table represents a single item.
+     * Defines constant values for the store database table
+     * Each entry in the table represents a single item
      */
     public static final class ItemEntry implements BaseColumns {
 
-        /**
-         * The content URI to access the item data in the provider
-         */
+        // Content URI to access the item data in the provider
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_STORE);
 
-        /**
-         * The MIME type of the {@link #CONTENT_URI} for a list of items.
-         */
+        // MIME type of the {@link #CONTENT_URI} for a list of items
         public static final String CONTENT_LIST_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_STORE;
 
-        /**
-         * The MIME type of the {@link #CONTENT_URI} for a single item.
-         */
+        // MIME type of the {@link #CONTENT_URI} for a single item
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_STORE;
 
-        /**
-         * Name of database table for store
-         */
+        // Name of database table for store
         public final static String TABLE_NAME = "store";
 
         public final static String COLUMN_ITEM_NAME = "name";
